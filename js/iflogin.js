a56da60bcfc99675dbb7e7ee0862fdc6e1bb6f4d
Alexander Cherkasov <alexandercherkasov@Alexanders-MacBook-Air.local>
2023-12-07 16:23:45 +0300

    refistration check

---
+++ js/iflogin.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    checkUserIdCookie(); // Вызов функции при загрузке страницы
+});
+function checkUserIdCookie() {
+    var userId = getCookieValue('userId'); // Функция для получения значения куки по имени
+    if (userId !== '') {
+        aaa = document.getElementById("login");
+        aaa.innerHtml = "Профиль";
+        aaa.href = "profile.html"
+    } 
+}
+
+function getCookieValue(cookieName) {
+    var name = cookieName + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return '';
+}
